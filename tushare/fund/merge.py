a7f7ad4221c992511496855d23c218a7eef11229
dlfkobe <1903152827@qq.com>
2023-02-01 20:57:03 +0800

    初步处理数据代码

--- tushare/fund/merge.py
+++ tushare/fund/merge.py
@@ -13,9 +13,24 @@ pro = ts.pro_api()
 
 # 获取单个基金的信息
  
-def get_fund(fund_code):    
+def get_fund_manager(fund_code):    
     # 获取基金经理信息，以及开始管理该基金的日期
     df_manager = pro.fund_manager(ts_code=fund_code)
+    # print(df_manager)
+    if df_manager.empty:
+       return [False,0]
+    else:
+        df_manager = df_manager.sort_values('begin_date',ascending=False)
+        # print(type(df_manager))
+        # for i in df_manager:
+        #   print(i)
+        one_person = df_manager.iloc[0]
+        print(list(one_person))
+        return [True,one_person]
+    
+def get_fund_company(fund_code):    
+    # 获取基金经理信息，以及开始管理该基金的日期
+    df_company = pro.fund_manager(ts_code=fund_code)
     # print(df_manager)
     if df_manager.empty:
        return [False,0]
@@ -32,27 +47,33 @@ def get_fund(fund_code):
 
 # 加载基金详情中csv，读取到基金代码
 path = "{}/公募基金列表1.csv".format(csv_dir)
-# 使用pandas读入
-data = pd.read_csv(path) #读取文件中所有数据
+# 结果
 result =[]
 # 遍历每一行
-for index,row in data.iterrows():
-    code = row['ts_code']
-    print(code)
-    flag,manager = get_fund(code)
-    if  not flag :
-        continue
-    print(manager)
-    a = list(row)
-    print(a)
-    a.extend(manager)
-    result.append(a)
-    # row.append(manager)
-    # r = pd.merge(row, manager,left_index=True,right_index=True)
-    # print("合并结果为：",r)
-    # pd.concat(result,r)
+headers = []
+
+with open(path, 'r',encoding='utf-8') as f:
+    reader = csv.reader(f)
+    headers_1 = next(reader)
+    headers_2 = ["ts_code","ann_date","name","gender","birth_year","edu","nationality","begin_date","end_date","resume"]
+    headers.extend(headers_1)
+    headers.extend(headers_2)
+    
+    for row in reader:
+        print(row)
+
+        code = row[1]
+        print(code)
+        flag,manager = get_fund_manager(code)
+        if  not flag :
+            continue
+        print(manager)
+        row.extend(manager)
+        result.append(row)
+    
+    
 print("结果为：\n",result)
+test = pd.DataFrame(columns=headers,data=result)
+test.to_csv('testcsv.csv',encoding='utf-8')
     
-    
-a = get_fund("1500011.SZ")
     
