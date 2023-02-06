f0a832a500c825e86d4ad69075c200ba60ed4ec9
dlfkobe <1903152827@qq.com>
2023-02-06 20:21:32 +0800

    数据处理完成

--- tushare/mydata/csv2json.py
+++ tushare/mydata/csv2json.py
@@ -23,13 +23,13 @@
 # 	jsonFile.write(jsonData)
 
 import json
-fo=open("./mydata/fund.csv","r",encoding="utf8")  #打开csv文件
+fo=open("./mydata/fund.csv","r",encoding="utf-8")  #打开csv文件
 ls=[]
 for line in fo:
     line=line.replace("\n","")  #将换行换成空
     ls.append(line.split(","))  #以，为分隔符
 fo.close()  #关闭文件流
-fw=open("./mydata/fund_data.json","w",encoding="utf8")  #打开json文件
+fw=open("./mydata/fund_data.json","w",encoding="utf-8")  #打开json文件
 for i in range(1,len(ls)):  #遍历文件的每一行内容，除了列名
     ls[i]=dict(zip(ls[0],ls[i]))  #ls[0]为列名，所以为key,ls[i]为value,
     #zip()是一个内置函数，将两个长度相同的列表组合成一个关系对
